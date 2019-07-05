086ef0e73e39252d447fef424b5b47f88069aecd
EC2 Default User <triha74@gmail.com>
2019-07-05 13:34:08 +0000

    style fixes

--- src/lambda_codebase/initial_commit/bootstrap_repository/deployment/lambda_codebase/initial_commit/pipelines_repository/adf-build/config.py
+++ src/lambda_codebase/initial_commit/bootstrap_repository/deployment/lambda_codebase/initial_commit/pipelines_repository/adf-build/config.py
@@ -1,3 +1,10 @@
+# Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: MIT-0
+
+"""
+Module used for loading Config from the config.yml.
+"""
+
 import os
 import yaml
 
@@ -5,8 +12,7 @@ class Config:
     def __init__(self, config_path=None):
         self.config_path = config_path or 'config.yml'
         with open(self.config_path, 'r') as stream:
-            self.config=yaml.load(stream, Loader=yaml.FullLoader)
-            
+            self.config = yaml.load(stream, Loader=yaml.FullLoader)
+
     def get_config(self, key, default=None):
         return self.config.get(key, default)
-            
