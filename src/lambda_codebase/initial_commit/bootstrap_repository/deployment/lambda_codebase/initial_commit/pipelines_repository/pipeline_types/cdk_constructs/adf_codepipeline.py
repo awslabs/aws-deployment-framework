12b74abfc07b9042adc78951f65d2685cf39b18a
Flynn <flynn.sbundy@gmail.com>
2019-08-01 16:34:52 +0200

    1.3.0

--- src/lambda_codebase/initial_commit/bootstrap_repository/deployment/lambda_codebase/initial_commit/pipelines_repository/pipeline_types/cdk_constructs/adf_codepipeline.py
+++ src/lambda_codebase/initial_commit/bootstrap_repository/deployment/lambda_codebase/initial_commit/pipelines_repository/pipeline_types/cdk_constructs/adf_codepipeline.py
@@ -34,21 +34,30 @@ def fetch_required_ssm_params(self, regions):
     for region in regions:
         output[region] = {
             "s3": _ssm.StringParameter.from_string_parameter_attributes(
-                    self, 
-                    'S3Bucket{0}'.format(region), 
-                    parameter_name='S3Bucket{0}'.format(region)
-                ),
+                    self,
+                    'S3Bucket{0}'.format(region),
+                    parameter_name='/cross_region/s3_regional_bucket/{0}'.format(region)
+                ).string_value,
             "kms": _ssm.StringParameter.from_string_parameter_attributes(
-                    self, 
+                    self,
                     'KMSKey{0}'.format(region), 
-                    parameter_name='KMSKey{0}'.format(region)
-                )
+                    parameter_name='/cross_region/kms_arn/{0}'.format(region)
+                ).string_value,
         }
+    output[ADF_DEPLOYMENT_REGION]["modules"] = _ssm.StringParameter.from_string_parameter_attributes(
+                    self, 
+                    'deployment_account_bucket', 
+                    parameter_name='deployment_account_bucket'
+                ).string_value
     return output
 
-def generate_build_env_variables(codebuild):
-    codebuild.BuildEnvironmentVariable.value = [{"Name": "test"}]
-    return codebuild
+def generate_build_env_variables(codebuild, shared_modules_bucket):
+    return {
+        "PYTHONPATH": codebuild.BuildEnvironmentVariable(value='./adf-build/python'), 
+        "ADF_PROJECT_NAME": codebuild.BuildEnvironmentVariable(value=ADF_PROJECT_NAME),
+        "S3_BUCKET_NAME": codebuild.BuildEnvironmentVariable(value=shared_modules_bucket),
+        "ACCOUNT_ID": codebuild.BuildEnvironmentVariable(value=core.Aws.ACCOUNT_ID)
+    }
 
 class Pipeline(core.Construct):
     def __init__(self, scope: core.Construct, id: str, params: dict, **kwargs):
@@ -56,53 +65,64 @@ class Pipeline(core.Construct):
 
         [_codepipeline_role_arn, _code_build_role_arn, _send_slack_notification_lambda_arn] = import_required_arns(import_arns)
         _ssm_parameters = fetch_required_ssm_params(self, params['regions'])
-        # _artifact_bucket = _s3.Bucket.from_bucket_name(self, 'deployment_region_bucket', 
-        #     bucket_name = params['parameter_store_client'].fetch_parameter('/cross_region/s3_regional_bucket/{0}'.format(os.environ["ADF_DEPLOYMENT_REGION"]))
-        # )
         _pipeline_args = {
-            "role": _codepipeline_role_arn,
+            "role": _iam.Role.from_role_arn(self, 'CodePipelineRole', role_arn=_codepipeline_role_arn),
             "restart_execution_on_update": params.get('restart_execution_on_update', False),
-            "pipeline_name": params['name']
+            "pipeline_name": params['name'],
+            "artifact_bucket": _s3.Bucket.from_bucket_name(self, 'ArtifactBucket', _ssm_parameters[ADF_DEPLOYMENT_REGION]["s3"])
         }
         if len(params['regions']) > 1:
-            _cross_region_buckets = { region:d["s3"] for [ region, d ] in fetch_required_ssm_params(self, params['regions']).items() }
+            _cross_region_buckets = { region:d["s3"] for [ region, d ] in _ssm_parameters.items() }
             _pipeline_args['cross_region_replication_buckets'] = _cross_region_buckets
 
-        print(**_pipeline_args)
-        # _artifact = _codepipeline.Artifact(artifact_name=ADF_PROJECT_NAME)
         pipeline = _codepipeline.Pipeline(self, "Pipeline",
             **_pipeline_args
         )
+        _input_artifact = _codepipeline.Artifact(artifact_name=ADF_PROJECT_NAME)
+        _output_artifact = _codepipeline.Artifact(artifact_name="{0}-build".format(ADF_PROJECT_NAME))
         _source = _codepipeline_actions.CodeCommitSourceAction(
             branch=params.get('branch_name', 'master'),
-            action_name="Source",
-            repository=params.get('project_name', params.get('repository_name')),
+            action_name='Source',
+            trigger=_codepipeline_actions.CodeCommitTrigger("POLL"),
+            repository=_codecommit.Repository.from_repository_arn(
+                self,
+                'SourceRepo',
+                repository_arn='arn:aws:codecommit:{0}:{1}:{2}'.format(
+                    ADF_DEPLOYMENT_REGION, 
+                    params.get('params')[0]['SourceAccountId'], 
+                    params.get('name', params.get('repository_name'))
+                )
+            ),
             run_order=1,
-            role=params.get('source_role', ADF_DEFAULT_SOURCE_ROLE),
-            output=["source"]
+            role=_iam.Role.from_role_arn(self, 'SourceRole', role_arn=params.get('source_role', ADF_DEFAULT_SOURCE_ROLE)),
+            output=_input_artifact
         )
         _env = _codebuild.BuildEnvironment(
-            build_image=params.get('image', _codebuild.LinuxBuildImage.UBUNTU_14_04_PYTHON_3_7_1),
-            compute_type=params.get('compute_type', _codebuild.ComputeType.SMALL),
-            environment_variables=generate_build_env_variables(_codebuild),
+            build_image=_codebuild.LinuxBuildImage.UBUNTU_14_04_PYTHON_3_7_1,
+            compute_type=_codebuild.ComputeType.SMALL,
+            environment_variables=generate_build_env_variables(_codebuild, _ssm_parameters[ADF_DEPLOYMENT_REGION]["modules"]),
             privileged=True
         )
+        _build_role = _iam.Role.from_role_arn(
+            self, 
+            'DefaultBuildRole', 
+            role_arn=ADF_DEFAULT_BUILD_ROLE
+        )
+
         _project = _codebuild.PipelineProject(self, 'Project', 
             environment=_env,
-            encryption_key=_ssm_parameters["KMSKey{0}".format(ADF_DEPLOYMENT_REGION)],
-            allow_all_outbound=True,
+            encryption_key=_kms.Key.from_key_arn(self, "DeploymentAccountKey", key_arn=_ssm_parameters[ADF_DEPLOYMENT_REGION]["kms"]),
             description="ADF CodeBuild Project for {0}".format(ADF_PROJECT_NAME),
             project_name=ADF_PROJECT_NAME,
-            timeout=ADF_DEFAULT_BUILD_TIMEOUT,
-            role=_code_build_role_arn
+            timeout=core.Duration.minutes(ADF_DEFAULT_BUILD_TIMEOUT),
+            role=_build_role
         )
         _build = _codepipeline_actions.CodeBuildAction(
             action_name="Build",
-            input="source",
+            input=_input_artifact,
             project=_project,
-            outputs="{0}-build".format(ADF_PROJECT_NAME),
+            outputs=[_output_artifact],
             type=_codepipeline_actions.CodeBuildActionType.BUILD,
-            role=params.get('build_role', ADF_DEFAULT_BUILD_ROLE),
             run_order=1
         )
         pipeline.add_stage(
