2278028fa615bd8cbf75601fa9e6bead414d298b
stemons <75492869+stemons@users.noreply.github.com>
2021-11-05 22:59:35 +0100

    Update src/lambda_codebase/initial_commit/bootstrap_repository/adf-build/shared/helpers/terraform/get_accounts.py
    remove comments
    
    Co-authored-by: Stewart Wallace <github@thestuhrer.net>

--- src/lambda_codebase/initial_commit/bootstrap_repository/adf-build/shared/helpers/terraform/get_accounts.py
+++ src/lambda_codebase/initial_commit/bootstrap_repository/adf-build/shared/helpers/terraform/get_accounts.py
@@ -40,7 +40,6 @@ def get_accounts_from_ous():
     parent_ou_id = None
     account_list = []
     organizations = get_boto3_client('organizations', f'arn:aws:sts::{master_acc_id}:role/OrganizationAccountAccessRole-readonly', 'getaccountID')
-    # organizations = get_boto3_client('organizations', 'arn:aws:sts::' + master_acc_id + ':role/Admin', 'getaccountID')
     # Read organization root id
     root_ids = []
     for id in paginator(organizations.list_roots):
