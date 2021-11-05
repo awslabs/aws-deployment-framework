a2eb1250cf5d64651c8c92d0e6a6d1bda1f879c7
stemons <75492869+stemons@users.noreply.github.com>
2021-11-05 22:59:03 +0100

    Update src/lambda_codebase/initial_commit/bootstrap_repository/adf-build/shared/helpers/terraform/get_accounts.py
    Co-authored-by: Stewart Wallace <github@thestuhrer.net>

--- src/lambda_codebase/initial_commit/bootstrap_repository/adf-build/shared/helpers/terraform/get_accounts.py
+++ src/lambda_codebase/initial_commit/bootstrap_repository/adf-build/shared/helpers/terraform/get_accounts.py
@@ -26,7 +26,7 @@ def get_accounts():
 
     print("Master Account ID: " + master_acc_id)
     # Assume a role into the Org Master role to get account ID's and emails
-    organizations = get_boto3_client('organizations', 'arn:aws:sts::' + master_acc_id + ':role/OrganizationAccountAccessRole-readonly', 'getaccountID')
+    organizations = get_boto3_client('organizations', f'arn:aws:sts::{master_acc_id}:role/OrganizationAccountAccessRole-readonly', 'getaccountID')
     # organizations = get_boto3_client('organizations', 'arn:aws:sts::' + master_acc_id + ':role/Admin', 'getaccountID')
     for account in paginator(organizations.list_accounts):
         if account['Status'] == 'ACTIVE':
