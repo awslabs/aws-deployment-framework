7440ae328048ef2d22a055597fc04861eba1218a
Stefano Montanelli <stemons+bitbucket@amazon.it>
2021-11-09 23:19:53 +0100

    added s3 public access block to terraform sample

--- samples/sample-terraform/tf/s3.tf
+++ samples/sample-terraform/tf/s3.tf
@@ -1,4 +1,11 @@
-resource "aws_s3_bucket" "b" {
+resource "aws_s3_bucket" "s3" {
   bucket = "my-tf-test-bucket-${var.TARGET_REGION}-${var.TARGET_ACCOUNT_ID}"
   acl    = "private"
+}
+
+resource "aws_s3_bucket_public_access_block" "s3-public-block" {
+  bucket = aws_s3_bucket.s3.id
+
+  block_public_acls   = true
+  block_public_policy = true
 }
