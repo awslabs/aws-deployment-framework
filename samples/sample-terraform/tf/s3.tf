17a6c69e9521a825bced3259b2a9f2de7aaf031f
stemons <75492869+stemons@users.noreply.github.com>
2021-11-19 20:28:40 +0100

    Update samples/sample-terraform/tf/s3.tf
    Co-authored-by: Simon Kok <sbkok@users.noreply.github.com>

--- samples/sample-terraform/tf/s3.tf
+++ samples/sample-terraform/tf/s3.tf
@@ -8,4 +8,6 @@ resource "aws_s3_bucket_public_access_block" "s3-public-block" {
 
   block_public_acls   = true
   block_public_policy = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
