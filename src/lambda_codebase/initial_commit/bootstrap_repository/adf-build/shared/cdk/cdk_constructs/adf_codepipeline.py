5d6bdb27735b8df8a1a45efc216bff07147d839c
Joris Conijn <jorisconijn@binx.io>
2022-03-08 12:08:10 +0100

    fix: do not use builtin name `input`

--- src/lambda_codebase/initial_commit/bootstrap_repository/adf-build/shared/cdk/cdk_constructs/adf_codepipeline.py
+++ src/lambda_codebase/initial_commit/bootstrap_repository/adf-build/shared/cdk/cdk_constructs/adf_codepipeline.py
@@ -371,9 +371,9 @@ class Action:
         ]
         if self.category == 'Deploy':
             for override in self.target.get('properties', {}).get('param_overrides', []):
-                input = _codepipeline.CfnPipeline.InputArtifactProperty(name=override.get('inputs', ''))
-                if self.provider == "CloudFormation" and override.get('inputs') and self.action_mode != "CHANGE_SET_EXECUTE" and input not in input_artifacts:
-                    input_artifacts.append(input)
+                _input = _codepipeline.CfnPipeline.InputArtifactProperty(name=override.get('inputs', ''))
+                if self.provider == "CloudFormation" and override.get('inputs') and self.action_mode != "CHANGE_SET_EXECUTE" and _input not in input_artifacts:
+                    input_artifacts.append(_input)
         return input_artifacts
 
     def _get_base_output_artifact_name(self):
