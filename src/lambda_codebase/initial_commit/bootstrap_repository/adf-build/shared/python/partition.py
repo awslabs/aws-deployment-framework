0f9f2baefbb3823ebc18aa6927b3b68c2691d52e
Simon Kok <sbkok@users.noreply.github.com>
2022-08-16 11:31:22 +0200

    Fix: Lookup correct Organization API Region (#485)
    * Fix: Lookup correct Organization API Region
    
    **Why?**
    
    At the moment, the organization API calls are made to the region where
    the organizations.py code is running.
    
    However, which region to use depends on the partition that is in use.
    For the gov-cloud regions, it should use the `us-gov-west-1` region.
    While the `aws` partition regions should use `us-east-1`.
    
    **What?**
    
    The code will determine the partition and return the correct region to
    use for the Organizations API.
    
    * Use boto3 Session to determine partition + throw error if unsupported
    
    **Why?**
    
    Use of the boto3 Session get_partition_for_region function allows us to
    rely on a supported and maintained function instead.
    
    Certain partitions are not supported by ADF yet. If such a partition is
    detected, we should throw an error to highlight this.
    
    **What?**
    Refactored to boto3 get_partition_for_region for partition lookup.
    
    Throwing error if the partition is not supported by ADF yet.

--- src/lambda_codebase/initial_commit/bootstrap_repository/adf-build/shared/python/partition.py
+++ src/lambda_codebase/initial_commit/bootstrap_repository/adf-build/shared/python/partition.py
@@ -1,6 +1,5 @@
 # Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: MIT-0
-
 """Partition.
 
 A partition is a group of AWS Regions. This module provides a helper function
@@ -9,6 +8,14 @@ on partitions, see:
 https://docs.aws.amazon.com/general/latest/gr/aws-arns-and-namespaces.html#genref-aws-service-namespaces
 """
 
+from boto3.session import Session
+
+COMPATIBLE_PARTITIONS = ['aws-us-gov', 'aws']
+
+
+class IncompatiblePartitionError(Exception):
+    pass
+
 
 def get_partition(region_name: str) -> str:
     """Given the region, this function will return the appropriate partition.
@@ -16,8 +23,25 @@ def get_partition(region_name: str) -> str:
     :param region_name: The name of the region (us-east-1, us-gov-west-1)
     :return: Returns the partition name as a string.
     """
+    partition = Session().get_partition_for_region(region_name)
+    if partition not in COMPATIBLE_PARTITIONS:
+        raise IncompatiblePartitionError(
+            f'The {partition} partition is not supported by this version of '
+            'ADF yet.'
+        )
 
-    if region_name.startswith('us-gov'):
-        return 'aws-us-gov'
+    return partition
 
-    return 'aws'
+
+def get_organization_api_region(region_name: str) -> str:
+    """
+    Given the current region, it will determine the partition and use
+    that to return the Organizations API region (us-east-1 or us-gov-west-1)
+
+    :param region_name: The name of the region (eu-west-1, us-gov-east-1)
+    :return: Returns the AWS Organizations API region to use as a string.
+    """
+    if get_partition(region_name) == 'aws-us-gov':
+        return 'us-gov-west-1'
+
+    return 'us-east-1'
