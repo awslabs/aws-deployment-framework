459623109332722aaa33727c9de6bb9ac80c6470
Simon Kok <mail@simonkok.com>
2022-09-16 15:28:28 +0200

    Forward CodePipeline Execution Id to Account Mgmt Creation SFN
    Step 2 of fixing #518.
    
    **Why?**
    
    As explained in #518, we need to forward the execution id of the CodePipeline
    that triggered the Account Management state machine so we can wait for to
    complete.
    
    **What?**
    
    Adding the CodePipeline execution identifier to the Step Functions
    State Machine invocation to enable tracing state machines in progress at
    CodeBuild execution time.

--- src/lambda_codebase/account_processing/process_account_files.py
+++ src/lambda_codebase/account_processing/process_account_files.py
@@ -11,6 +11,7 @@ import json
 import os
 import tempfile
 import logging
+from typing import Any, TypedDict
 import yaml
 
 from yaml.error import YAMLError
@@ -29,7 +30,21 @@ ACCOUNT_MANAGEMENT_STATEMACHINE = os.getenv(
 )
 
 
-def get_details_from_event(event: dict):
+class S3ObjectLocation(TypedDict):
+    bucket_name: str
+    key: str
+
+
+class AccountFileData(TypedDict):
+    """
+    Class used to return YAML account file data and its related
+    metadata like the execution_id of the CodePipeline that uploaded it.
+    """
+    content: Any
+    execution_id: str
+
+
+def get_details_from_event(event: dict) -> S3ObjectLocation:
     s3_details = event.get("Records", [{}])[0].get("s3")
     if not s3_details:
         raise ValueError("No S3 Event details present in event trigger")
@@ -41,33 +56,40 @@ def get_details_from_event(event: dict):
     }
 
 
-def get_file_from_s3(s3_object: dict, s3_resource: boto3.resource):
+def get_file_from_s3(
+    s3_object_location: S3ObjectLocation,
+    s3_resource: boto3.resource,
+) -> AccountFileData:
     try:
         LOGGER.debug(
-            "Reading YAML from S3: %s",
-            json.dumps(s3_object, indent=2) if LOGGER.isEnabledFor(logging.DEBUG) else "--data-hidden--"
+            "Reading YAML from S3: %s from %s",
+            s3_object_location.get('object_key'),
+            s3_object_location.get('bucket_name'),
         )
-        s3_object = s3_resource.Object(**s3_object)
+        s3_object = s3_resource.Object(**s3_object_location)
         with tempfile.TemporaryFile(mode='w+b') as file_pointer:
             s3_object.download_fileobj(file_pointer)
 
             # Move pointer to the start of the file
             file_pointer.seek(0)
 
-            return yaml.safe_load(file_pointer)
+            return {
+                "content": yaml.safe_load(file_pointer),
+                "execution_id": s3_object.metadata.get("execution_id"),
+            }
     except ClientError as error:
         LOGGER.error(
             "Failed to download %s from %s, due to %s",
-            s3_object.get('object_key'),
-            s3_object.get('bucket_name'),
+            s3_object_location.get('object_key'),
+            s3_object_location.get('bucket_name'),
             error,
         )
         raise
     except YAMLError as yaml_error:
         LOGGER.error(
             "Failed to parse YAML file: %s from %s, due to %s",
-            s3_object.get('object_key'),
-            s3_object.get('bucket_name'),
+            s3_object_location.get('object_key'),
+            s3_object_location.get('bucket_name'),
             yaml_error,
         )
         raise
@@ -89,7 +111,9 @@ def process_account(account_lookup, account):
 
 
 def process_account_list(all_accounts, accounts_in_file):
-    account_lookup = {account["Name"]: account["Id"] for account in all_accounts}
+    account_lookup = {
+        account["Name"]: account["Id"] for account in all_accounts
+    }
     processed_accounts = list(map(
         lambda account: process_account(
             account_lookup=account_lookup,
@@ -100,24 +124,43 @@ def process_account_list(all_accounts, accounts_in_file):
     return processed_accounts
 
 
-def start_executions(sfn_client, processed_account_list):
+def start_executions(
+    sfn_client,
+    processed_account_list,
+    codepipeline_execution_id: str,
+    request_id: str,
+):
+    run_id = (
+        f"{codepipeline_execution_id, 'no-exec-id'}-"
+        f"{request_id}"
+    )
     LOGGER.info(
-        "Invoking Account Management State Machine (%s)",
+        "Invoking Account Management State Machine (%s) -> %s",
         ACCOUNT_MANAGEMENT_STATEMACHINE,
+        run_id,
     )
     for account in processed_account_list:
-        LOGGER.debug(f"Payload: {account}")
+        full_account_name = account.get('account_full_name', 'no-account-name')
+        sfn_execution_name = f"{full_account_name}-{run_id}"
+
+        LOGGER.debug(
+            "Payload for %s: %s",
+            sfn_execution_name,
+            account,
+        )
         sfn_client.start_execution(
             stateMachineArn=ACCOUNT_MANAGEMENT_STATEMACHINE,
+            name=sfn_execution_name,
             input=f"{json.dumps(account)}",
         )
 
 
-def lambda_handler(event, _):
+def lambda_handler(event, context):
     """Main Lambda Entry point"""
     LOGGER.debug(
         "Processing event: %s",
-        json.dumps(event, indent=2) if LOGGER.isEnabledFor(logging.DEBUG) else "--data-hidden--"
+        json.dumps(event, indent=2) if LOGGER.isEnabledFor(logging.DEBUG)
+        else "--data-hidden--"
     )
     sfn_client = boto3.client("stepfunctions")
     s3_resource = boto3.resource("s3")
@@ -127,8 +170,13 @@ def lambda_handler(event, _):
 
     processed_account_list = process_account_list(
         all_accounts=all_accounts,
-        accounts_in_file=account_file.get("accounts"),
+        accounts_in_file=account_file.get("content", {}).get("accounts"),
     )
 
-    start_executions(sfn_client, processed_account_list)
+    start_executions(
+        sfn_client,
+        processed_account_list,
+        codepipeline_execution_id=account_file.get("execution_id"),
+        request_id=context.aws_request_id,
+    )
     return event
