43e1cfdd735d3f7ba1c0635ef47e216c29eca84c
Stewart Wallace <stewwall@amazon.com>
2022-09-20 11:38:14 +0100

    Adding in default values for extensions

--- src/lambda_codebase/initial_commit/bootstrap_repository/adf-build/tests/test_config.py
+++ src/lambda_codebase/initial_commit/bootstrap_repository/adf-build/tests/test_config.py
@@ -4,6 +4,7 @@
 # pylint: skip-file
 
 import os
+from pickle import TRUE
 from errors import InvalidConfigError
 
 from pytest import fixture, raises
@@ -16,16 +17,18 @@ def cls():
     parameter_store = Mock()
     return Config(
         parameter_store=parameter_store,
-        config_path='{0}/stubs/stub_adfconfig.yml'.format(
+        config_path="{0}/stubs/stub_adfconfig.yml".format(
             os.path.dirname(os.path.realpath(__file__))
-        )
+        ),
     )
 
 
 def test_validation(cls):
-    assert cls.config.get(
-        'main-notification-endpoint')[0].get('target') == 'john@example.com'
-    assert cls.config.get('moves')[0].get('action') == 'remove-base'
+    assert (
+        cls.config.get("main-notification-endpoint")[0].get("target")
+        == "john@example.com"
+    )
+    assert cls.config.get("moves")[0].get("action") == "remove-base"
 
 
 def test_validation_list_deployment_target(cls):
@@ -41,33 +44,59 @@ def test_validation_list_deployment_account_target(cls):
 
 
 def test_raise_validation_remove_moves(cls):
-    cls.config_contents.get('config').pop('moves', None)
+    cls.config_contents.get("config").pop("moves", None)
     with raises(InvalidConfigError):
         assert cls._parse_config()
 
 
 def test_raise_validation_remove_roles(cls):
-    cls.config_contents.get('roles', None).pop('cross-account-access', None)
+    cls.config_contents.get("roles", None).pop("cross-account-access", None)
     with raises(InvalidConfigError):
         assert cls._parse_config()
 
 
 def test_raise_validation_remove_deployment_target_region(cls):
-    cls.config_contents.get('regions', None).pop('deployment-account', None)
+    cls.config_contents.get("regions", None).pop("deployment-account", None)
     with raises(InvalidConfigError):
         assert cls._parse_config()
 
 
 def test_raise_validation_length_deployment_target_region(cls):
     cls.config_contents["regions"]["deployment-account"] = [
-        'region1',
-        'region2',
+        "region1",
+        "region2",
     ]
     with raises(InvalidConfigError):
         assert cls._parse_config()
 
 
 def test_raise_validation_organizations_scp(cls):
-    cls.config_contents['config']['scp']['keep-default-scp'] = 'blah'
+    cls.config_contents["config"]["scp"]["keep-default-scp"] = "blah"
     with raises(InvalidConfigError):
         assert cls._parse_config()
+
+
+def test_extensions_default_configuration(cls):
+    cls._parse_config()
+    assert cls.extensions == {"terraform": {"enabled": False}}
+
+
+def test_extensions_default_configuration_with_custom(cls):
+    cls.config_contents["extensions"] = {"another_extensions": {"enabled": True}}
+    cls._parse_config()
+    assert cls.extensions == {
+        "another_extensions": {"enabled": True},
+        "terraform": {"enabled": False},
+    }
+
+
+def test_extensions_default_configuration_wont_override_custom(cls):
+    cls.config_contents["extensions"] = {
+        "another_extensions": {"enabled": True},
+        "terraform": {"enabled": True},
+    }
+    cls._parse_config()
+    assert cls.extensions == {
+        "another_extensions": {"enabled": True},
+        "terraform": {"enabled": True},
+    }
