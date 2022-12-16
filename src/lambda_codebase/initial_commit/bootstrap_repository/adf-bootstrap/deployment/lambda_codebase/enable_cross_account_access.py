ef3de13df940f0ab199b48b3c48d01b5e4210e8a
Simon Kok <mail@simonkok.com>
2022-12-16 15:21:19 +0100

    Reduce number of cross-account access IAM API calls + add exp backoff retries
    **Why?**
    
    In the EnableCrossAccountAccess process, it would call PutRolePolicy even when
    the policy was not modified.
    Additionally, it would assume into the target account using STS for each
    region, while only making API calls to the global IAM service.
    
    **What?**
    
    * The IAM Class comments stated it was used across ADF, this is not the case.
    * The implementation of the IAM class was very specific to enabling cross
      account access. Considering it is not used anywhere else, it is moved
      to the Lambda function code base in the deployment account.
    * Renamed the IAM file and class to reflect what it is managing.
    * Updated the implementation to rely on OOP best practices, creating different
      instances when we try to update another instead of overwriting the parameters
      using a singleton approach.
    * Added tests to validate that the right permissions are set.
    * Updated the retry logic in the Step Function state machine to retry on
      ClientErrors, with a higher wait period and exponential backoff.

--- src/lambda_codebase/initial_commit/bootstrap_repository/adf-bootstrap/deployment/lambda_codebase/enable_cross_account_access.py
+++ src/lambda_codebase/initial_commit/bootstrap_repository/adf-bootstrap/deployment/lambda_codebase/enable_cross_account_access.py
@@ -11,13 +11,12 @@ This runs as part of Step Functions on the Deployment Account
 import os
 import boto3
 
-
 from botocore.exceptions import ClientError
 from logger import configure_logger
 from parameter_store import ParameterStore
 from sts import STS
-from iam import IAM
 from partition import get_partition
+from .iam_cfn_deploy_role_policy import IAMCfnDeployRolePolicy
 
 
 KEY_ID = os.environ["KMS_KEY_ID"]
@@ -25,38 +24,49 @@ S3_BUCKET = os.environ["S3_BUCKET_NAME"]
 REGION_DEFAULT = os.getenv("AWS_REGION")
 LOGGER = configure_logger(__name__)
 
+# Target Role Policies are updated in target accounts
+TARGET_ROLE_POLICIES = {
+    'adf-cloudformation-deployment-role': [
+        'adf-cloudformation-deployment-role-policy-kms',
+    ],
+    "adf-cloudformation-role": [
+        "adf-cloudformation-role-policy",
+        "adf-cloudformation-role-policy-s3",
+        "adf-cloudformation-role-policy-kms",
+    ],
+}
 
-def update_iam(role, s3_buckets, kms_key_arns, role_policies):
-    iam = IAM(role.client("iam"))
-    iam.update_iam_roles(s3_buckets, kms_key_arns, role_policies)
+# Role Policies are updated in the deployment account.
+DEPLOYMENT_ROLE_POLICIES = {
+    "adf-codepipeline-role": [
+        "adf-codepipeline-role-policy",
+        "adf-codepipeline-role-policy-s3",
+        "adf-codepipeline-role-policy-kms",
+    ],
+    "adf-cloudformation-deployment-role": [
+        "adf-cloudformation-deployment-role-policy",
+    ],
+    "adf-cloudformation-role": [
+        "adf-cloudformation-role-policy",
+    ],
+}
 
 
 def lambda_handler(event, _):
-    # Target Role Policies are updated in target accounts
-    target_role_policies = {
-        'adf-cloudformation-deployment-role': (
-            'adf-cloudformation-deployment-role-policy-kms'
-        ),
-        "adf-cloudformation-role": [
-            "adf-cloudformation-role-policy",
-            "adf-cloudformation-role-policy-s3",
-            "adf-cloudformation-role-policy-kms",
-        ],
-    }
+    """
+    Lambda handler of the enable cross-account access orchestrator.
+    This process will ensure that the deployment account and target regions
+    are able to use the correct ADF S3 buckets and KMS keys.
 
-    # Role Policies are updated in the deployment account.
-    role_policies = {
-        "adf-codepipeline-role": [
-            "adf-codepipeline-role-policy",
-            "adf-codepipeline-role-policy-s3",
-            "adf-codepipeline-role-policy-kms",
-        ],
-        "adf-cloudformation-deployment-role": (
-            "adf-cloudformation-deployment-role-policy"
-        ),
-        "adf-cloudformation-role": "adf-cloudformation-role-policy",
-    }
+    Args:
+        event (any): The input event submitted to AWS Lambda. This event will
+            hold the deployment_account_region, target regions, and account id
+            that should be configured.
 
+    Returns:
+        event (any): The input event that was submitted is passed forward, so
+            the next step in the State Machine is able to use the data too.
+    """
     sts = STS()
     partition = get_partition(REGION_DEFAULT)
 
@@ -64,10 +74,33 @@ def lambda_handler(event, _):
         region=event.get("deployment_account_region"), role=boto3
     )
     account_id = event.get("account_id")
+    try:
+        role_arn_to_assume = (
+            f'arn:{partition}:iam::{account_id}:'
+            f'role/adf-cloudformation-deployment-role'
+        )
+        target_account_role = sts.assume_cross_account_role(
+            role_arn_to_assume,
+            'base_cfn_role'
+        )
+        LOGGER.debug("Role has been assumed for %s", account_id)
+    except ClientError as err:
+        LOGGER.error(
+            "Could not assume into account %s with role %s -> error: %s.",
+            role_arn_to_assume,
+            account_id,
+            err,
+            exc_info=True,
+        )
+        raise
+
     kms_key_arns = []
     s3_buckets = []
     for region in list(
-        set([event.get("deployment_account_region")] + event.get("regions", []))
+        set(
+            [event.get("deployment_account_region")]
+            + event.get("regions", [])
+        )
     ):
         kms_key_arn = parameter_store.fetch_parameter(
             f"/cross_region/kms_arn/{region}"
@@ -77,25 +110,18 @@ def lambda_handler(event, _):
             f"/cross_region/s3_regional_bucket/{region}"
         )
         s3_buckets.append(s3_bucket)
-        try:
-            role = sts.assume_cross_account_role(
-                (
-                    f'arn:{partition}:iam::{account_id}:'
-                    f'role/adf-cloudformation-deployment-role'
-                ),
-                'base_cfn_role'
-            )
-            LOGGER.debug("Role has been assumed for %s", account_id)
-            update_iam(role, s3_bucket, kms_key_arn, target_role_policies)
-        except ClientError as err:
-            LOGGER.debug(
-                "%s could not be assumed (%s), continuing",
-                account_id,
-                err,
-                exc_info=True,
-            )
-            continue
+        IAMCfnDeployRolePolicy.update_iam_role_policies(
+            target_account_role.client("iam"),
+            [s3_bucket],  # Only the S3 bucket for this region
+            [kms_key_arn],  # Only the KMS Key for this region
+            TARGET_ROLE_POLICIES,
+        )
 
-    update_iam(boto3, s3_buckets, kms_key_arns, role_policies)
+    IAMCfnDeployRolePolicy.update_iam_role_policies(
+        boto3.client("iam"),
+        s3_buckets,
+        kms_key_arns,
+        DEPLOYMENT_ROLE_POLICIES,
+    )
 
     return event
---
+++ src/lambda_codebase/initial_commit/bootstrap_repository/adf-bootstrap/deployment/lambda_codebase/iam_cfn_deploy_role_policy.py
@@ -0,0 +1,182 @@
+# Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: MIT-0
+
+"""
+CloudFormation Deployment Role Policy used to manage cross account
+access.
+"""
+
+import json
+import os
+
+from logger import configure_logger
+from partition import get_partition
+
+LOGGER = configure_logger(__name__)
+REGION_DEFAULT = os.getenv('AWS_REGION')
+PARTITION = get_partition(REGION_DEFAULT)
+
+
+class IAMCfnDeployRolePolicy:
+    """
+    Class to manage an CloudFormation Deployment IAM Role Policy.
+    Used to update the cross-account CloudFormation Deployment role.
+    """
+
+    def __init__(self, client, role_name, policy_name):
+        """
+        Create a new IAM Role Policy instance.
+
+        Args:
+            client (boto3.IAM.client): The boto3 IAM client.
+            role_name (str):           The role name.
+            policy_name (str):         The policy name.
+            policy_document (dict):    The policy document.
+        """
+        self.client = client
+        self.role_name = role_name
+        self.policy_name = policy_name
+
+        LOGGER.debug('Fetching policy %s for role %s', policy_name, role_name)
+        role_policy_result = client.get_role_policy(
+            RoleName=role_name,
+            PolicyName=policy_name,
+        )
+        self.policy_changed = False
+        self.policy_document = role_policy_result['PolicyDocument']
+
+    def _get_statement(self, statement_id):
+        s3_statements = list(filter(
+            lambda stmt: stmt['Sid'] == statement_id,
+            self.policy_document.get('Statement', {})
+        ))
+        if len(s3_statements) == 1:
+            return s3_statements[0]
+
+        if len(s3_statements) > 1:
+            raise Exception(
+                f'Found multiple {statement_id} statements in '
+                f'Role {self.role_name} Policy {self.policy_name}.'
+            )
+        raise Exception(
+            f'Statement {statement_id} was not found in '
+            f'Role {self.role_name} Policy {self.policy_name}.'
+        )
+
+    def grant_access_to_s3_buckets(self, bucket_names):
+        """
+        Updates the IAM Role Policy to grant access to the specified S3 Bucket.
+        Allowing target account access to the deployment resources.
+
+        Args:
+            bucket_name (list[str]): The bucket names to grant access to.
+        """
+        LOGGER.debug(
+            'calling grant_s3_buckets_access for bucket_names %s',
+            bucket_names,
+        )
+        if len(bucket_names) == 0:
+            return
+        statement = self._get_statement('S3')
+        for bucket_name in bucket_names:
+            if f"arn:{PARTITION}:s3:::{bucket_name}" in statement['Resource']:
+                # The bucket is in the resource statement already, great!
+                continue
+
+            LOGGER.info(
+                'Updating Role %s policy %s to access S3://%s',
+                self.role_name,
+                self.policy_name,
+                bucket_name,
+            )
+            self.policy_changed = True
+            if not isinstance(statement['Resource'], list):
+                statement['Resource'] = [statement['Resource']]
+            statement['Resource'].append(
+                f"arn:{PARTITION}:s3:::{bucket_name}",
+            )
+            statement['Resource'].append(
+                f"arn:{PARTITION}:s3:::{bucket_name}/*",
+            )
+
+    def grant_access_to_kms_keys(self, kms_key_arns):
+        """
+        Updates the CloudFormation Deployment Role to allow use of the
+        given KMS Key Arns.
+
+        Args:
+            kms_key_arns (list[str]): The KMS Key Arns to grant access to.
+        """
+        LOGGER.debug(
+            'calling grant_usage_on_kms_keys for key arns %s',
+            kms_key_arns,
+        )
+        if len(kms_key_arns) == 0:
+            return
+        statement = self._get_statement('KMS')
+        for kms_key_arn in kms_key_arns:
+            if kms_key_arn in statement['Resource']:
+                # The Key is in the resource statement already, great!
+                continue
+            LOGGER.info(
+                'Updating Role %s Policy %s to be able to access %s',
+                self.role_name,
+                self.policy_name,
+                kms_key_arn,
+            )
+            self.policy_changed = True
+            if not isinstance(statement['Resource'], list):
+                statement['Resource'] = [statement['Resource']]
+            statement['Resource'].append(kms_key_arn)
+
+    def save(self):
+        """
+        Save the role policy if it was modified.
+        """
+        if self.policy_changed is False:
+            LOGGER.debug(
+                "Request to save the policy %s, while no changes were made.",
+                self.policy_name,
+            )
+            return
+        self.client.put_role_policy(
+            RoleName=self.role_name,
+            PolicyName=self.policy_name,
+            PolicyDocument=json.dumps(self.policy_document)
+        )
+        self.policy_changed = False
+
+    @staticmethod
+    def update_iam_role_policies(
+        client,
+        s3_bucket_names,
+        kms_key_arns,
+        role_policies,
+    ):
+        """
+        Update the IAM CloudFormation Deployment Role Policies to grant
+        access to the given list of S3 buckets and KMS Keys.
+
+        Args:
+            client (boto3.IAM.client): The IAM boto3 client.
+
+            s3_bucket_names (list[str]): The names of the S3 buckets that
+                should be granted access to.
+
+            kms_key_arns (list[str]): The Arns of the KMS Keys that should be
+                granted access to.
+
+            role_policies (dict[str, list[str]): The role policies to update.
+                Where the key is the role name and the value captures the list
+                of policies names to update.
+        """
+        for role_name, policy_names in role_policies.items():
+            for policy_name in policy_names:
+                iam_role_policy = IAMCfnDeployRolePolicy(
+                    client,
+                    role_name,
+                    policy_name,
+                )
+                iam_role_policy.grant_access_to_s3_buckets(s3_bucket_names)
+                iam_role_policy.grant_access_to_kms_keys(kms_key_arns)
+                iam_role_policy.save()
