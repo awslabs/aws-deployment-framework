5de05bba763606aa1552dc9d6f55c80dd3dc239a
AndyEfaa <73257849+AndyEfaa@users.noreply.github.com>
2023-08-08 09:41:27 +0200

    feat: introduce-variable-org-stage (#636)
    * feat: introduce-variable-org-stage
    
    * Add tests for main configuration parameters
    
    * Update docs/admin-guide.md
    
    * Fix linting issues
    
    ---------
    
    Co-authored-by: Andreas Falkenberg <falkena@amazon.com>
    Co-authored-by: Simon Kok <mail@simonkok.com>
    Co-authored-by: Simon Kok <sbkok@users.noreply.github.com>

--- src/lambda_codebase/initial_commit/bootstrap_repository/adf-build/main.py
+++ src/lambda_codebase/initial_commit/bootstrap_repository/adf-build/main.py
@@ -56,6 +56,7 @@ ACCOUNT_BOOTSTRAPPING_STATE_MACHINE_ARN = os.environ.get(
     "ACCOUNT_BOOTSTRAPPING_STATE_MACHINE_ARN"
 )
 ADF_DEFAULT_SCM_FALLBACK_BRANCH = 'master'
+ADF_DEFAULT_ORG_STAGE = "none"
 LOGGER = configure_logger(__name__)
 
 
@@ -120,8 +121,8 @@ def prepare_deployment_account(sts, deployment_account_id, config):
         f'{config.cross_account_access_role}',
         'master'
     )
-    for region in list(
-            set([config.deployment_account_region] + config.target_regions)):
+    for region in sorted(list(
+            set([config.deployment_account_region] + config.target_regions))):
         deployment_account_parameter_store = ParameterStore(
             region,
             deployment_account_role
@@ -149,6 +150,13 @@ def prepare_deployment_account(sts, deployment_account_id, config):
         config.config.get('scm', {}).get(
             'default-scm-branch',
             ADF_DEFAULT_SCM_FALLBACK_BRANCH,
+        )
+    )
+    deployment_account_parameter_store.put_parameter(
+        '/adf/org/stage',
+        config.config.get('org', {}).get(
+            'stage',
+            ADF_DEFAULT_ORG_STAGE,
         )
     )
     auto_create_repositories = config.config.get(
