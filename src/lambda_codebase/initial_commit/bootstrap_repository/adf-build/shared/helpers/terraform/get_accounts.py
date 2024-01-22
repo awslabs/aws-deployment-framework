d38de346cbc0f163e17126e836fe58458743170e
Igor D'Astolfo <igordust@amazon.it>
2024-01-22 15:59:31 +0100

    Switched to resourcegroupstaggingapi for tag filtering

--- src/lambda_codebase/initial_commit/bootstrap_repository/adf-build/shared/helpers/terraform/get_accounts.py
+++ src/lambda_codebase/initial_commit/bootstrap_repository/adf-build/shared/helpers/terraform/get_accounts.py
@@ -11,6 +11,7 @@ import os
 import boto3
 from paginator import paginator
 from partition import get_partition
+from partition import get_organization_api_region
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
@@ -25,6 +26,7 @@ REGION_DEFAULT = os.environ["AWS_REGION"]
 PARTITION = get_partition(REGION_DEFAULT)
 sts = boto3.client('sts')
 ssm = boto3.client('ssm')
+organizations = boto3.client('organizations')
 response = ssm.get_parameter(Name='cross_account_access_role')
 CROSS_ACCOUNT_ACCESS_ROLE = response['Parameter']['Value']
 
@@ -97,42 +99,38 @@ def get_accounts():
 
 
 def get_accounts_from_tags():
-    accounts = get_accounts()
     tag_filters = []
-    for tag in TARGET_TAGS.split(','):
-        tag_name = tag.split('=')[0]
-        tag_value = tag.split('=')[1]
+    for tags in TARGET_TAGS.split(";"):
+        tag_name = tags.split(",", 1)[0].split("=")[1]
+        tag_values = tags.split(",", 1)[1].split("=")[1].split(",")
         tag_filters.append({
             "Key": tag_name,
-            "Value": tag_value})
-
+            "Values": tag_values})
     LOGGER.info(
         "Tag filters %s",
         tag_filters
     )
-
-    organizations = get_boto3_client(
-        'organizations',
+    organization_api_region = get_organization_api_region(REGION_DEFAULT)
+    print(organization_api_region)
+    tags_client = get_boto3_client(
+        'resourcegroupstaggingapi',
         (
             f'arn:{PARTITION}:sts::{MANAGEMENT_ACCOUNT_ID}:role/'
             f'{CROSS_ACCOUNT_ACCESS_ROLE}-readonly'
         ),
-        'getaccountIDs',
+        'getaccountIDsFromTags',
+        region_name=organization_api_region,
     )
-    filtered_accounts = []
-    for account in accounts:
-        tags = account['Tags'] = organizations.list_tags_for_resource(
-            ResourceId=account['AccountId']
-        )['Tags']
-        for tag_filter in tag_filters:
-            found = list(filter(lambda item: (
-                item["Key"] == tag_filter["Key"] and item["Value"] == tag_filter["Value"]), tags))
-            if len(found) > 0:
-                LOGGER.info(
-                    f"{account['AccountId']} matched {tag_filter['Key']}={tag_filter['Value']}")
-                filtered_accounts.append(account)
-                break
-    return filtered_accounts
+    account_ids = []
+    for resource in paginator(
+        tags_client.get_resources,
+        TagFilters=tag_filters,
+        ResourceTypeFilters=["organizations"],
+    ):
+        arn = resource["ResourceARN"]
+        account_id = arn.split("/")[::-1][0]
+        account_ids.append({"AccountId": account_id})
+    return account_ids
 
 
 def get_accounts_from_ous():
@@ -187,7 +185,7 @@ def get_accounts_from_ous():
     return account_list
 
 
-def get_boto3_client(service, role, session_name):
+def get_boto3_client(service, role, session_name, region_name=''):
     role = sts.assume_role(
         RoleArn=role,
         RoleSessionName=session_name,
@@ -198,7 +196,10 @@ def get_boto3_client(service, role, session_name):
         aws_secret_access_key=role['Credentials']['SecretAccessKey'],
         aws_session_token=role['Credentials']['SessionToken']
     )
-    return session.client(service)
+    if region_name != '':
+        return session.client(service, region_name=region_name)
+    else:
+        return session.client(service)
 
 
 def get_account_recursive(org_client: boto3.client, ou_id: str, path: str) -> list:
