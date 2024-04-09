3517078cb424361c63cf7654fb8c0363e45d1cae
Simon Kok <sbkok@users.noreply.github.com>
2024-04-09 12:47:09 +0200

    Update CDK v2.136, SAM CLI 1.114, and others (#715)
    * Update CDK v2.136, SAM CLI 1.114, and others
    
    **Why?**
    
    To catch-up with the latest enhancements made in CDK and SAM CLI.
    
    **What?**
    
    Updated:
    
    * CDK to v2.136.0
    * SAM CLI to v1.114.0
    * And other packages to the most recent version that is compatible
    
    * Use yield-from as suggested by pylint

--- src/lambda_codebase/initial_commit/bootstrap_repository/adf-build/shared/python/paginator.py
+++ src/lambda_codebase/initial_commit/bootstrap_repository/adf-build/shared/python/paginator.py
@@ -11,5 +11,4 @@ def paginator(method, **kwargs):
     client = method.__self__
     iterator = client.get_paginator(method.__name__)
     for page in iterator.paginate(**kwargs).result_key_iters():
-        for result in page:
-            yield result
+        yield from page
