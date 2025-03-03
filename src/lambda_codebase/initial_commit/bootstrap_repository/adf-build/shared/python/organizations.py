b5e98cbfa7a19ef127072b76ebc25ca02d58a0a6
Igor D'Astolfo <130134134+igordust@users.noreply.github.com>
2025-03-03 09:04:50 +0100

    Update src/lambda_codebase/initial_commit/bootstrap_repository/adf-build/shared/python/organizations.py
    Co-authored-by: Simon Kok <sbkok@users.noreply.github.com>

--- src/lambda_codebase/initial_commit/bootstrap_repository/adf-build/shared/python/organizations.py
+++ src/lambda_codebase/initial_commit/bootstrap_repository/adf-build/shared/python/organizations.py
@@ -466,7 +466,7 @@ class Organizations:  # pylint: disable=R0904
         ]
 
     def list_organizational_units_for_parent(self, parent_ou):
-        print(f'looking for children in {parent_ou}')
+        LOGGER.debug('Looking for children in %s', parent_ou)
         cache_key = f'children_{parent_ou}'
         if not self.cache.exists(cache_key):
             LOGGER.debug(f'Cache MISS for children of OU {parent_ou}')
